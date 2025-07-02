eda1952fc3443f0b9ebf9c700edd6f4b905db954
Mikaël Sévigny <sevignator@hotmail.com>
2025-07-01 21:42:51 -0400

    Add solution to 01-04

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,34 @@
+import { createInterface } from "node:readline/promises";
+
 export function cleanInput(input: string): string[] {
-  const words = input.toLowerCase().trim().split(/\s+/);
-  return words;
+  const words = input.toLowerCase().trim().split(" ");
+  return words.filter((word) => word !== "");
+}
+
+export async function startREPL() {
+  // Generate the "readline" interface
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  // Display the prompt to the user
+  readline.prompt();
+
+  // Listen for user input
+  readline.on("line", (input) => {
+    const words = cleanInput(input);
+
+    // Exit early if no input was provided
+    if (words.length === 0) {
+      readline.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${words[0]}`);
+    readline.prompt();
+  });
+
+  return;
 }
